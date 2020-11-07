011976f087c4123eb7cc4d45049678a3b6223120
mckib2 <nicholas.bgp@gmail.com>
2020-11-06 18:30:29 -0700

    change includes api; resolves issue #3

--- clangTooling/__init__.py
+++ clangTooling/__init__.py
@@ -1,4 +1,8 @@
 '''clangTooling libraries and headers.'''
 
 from .lib import library_dir
-from .include import include_dir
+from .include import (
+    header_dir,
+    llvm_includes,
+    clang_includes,
+)
--- clangTooling/include/__init__.py
+++ clangTooling/include/__init__.py
@@ -3,6 +3,24 @@
 import pathlib
 
 
-def include_dir() -> pathlib.Path:
+def header_dir() -> pathlib.Path:
     '''Location of header files.'''
     return pathlib.Path(__file__).parent / 'headers/'
+
+
+def llvm_includes() -> list(pathlib.Path):
+    '''Include directories for LLVM.'''
+    hdrs = header_dir()
+    return [
+        hdrs / 'llvm/include',
+        hdrs / 'build/include',
+    ]
+
+
+def clang_includes() -> list(pathlib.Path):
+    '''Include directories for clang.'''
+    hdrs = header_dir()
+    return [
+        hdrs / 'clang/include',
+        hdrs / 'build/tools/clang/include',
+    ]
